0131c5e061cf23e6dac5412f79741f41344a842d
Dulas <abdullahtshabalala@gmail.com>
2025-06-02 16:19:38 +0200

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,32 @@
+// series.js
+const seriesData = [
+  {
+    title: "Bleach:TYBW P2",
+    image: "Images/cour2.jpeg",
+    description: "A battle for the soul of the universe continues.",
+    genres: ["Action", "Fantasy", "Anime"],
+    episodes: [
+      { title: "Episode 1", link: "https://drive.google.com/file/d/10D0xtKf_CPz5ODJAiX2A5wfyCg57NcIg/preview" },
+      { title: "Episode 2", link: "https://drive.google.com/file/d/1-K6_2qaz1ZCz7u54FSnNX-hPq3qbqmZf/preview" },
+      { title: "Episode 3", link: "https://drive.google.com/file/d/1-TQNzstys6szmuu-5iPmnxmZ6-oznFc2/preview" },
+    ]
+  },
+  {
+    title: "Konosuba (s1)",
+    image: "Images/Cover4.jpg",
+    description: "An intergalactic adventure across unknown planets.",
+    genres: ["Comedy", "Fantasy", "Isekai", "Anime"],
+    episodes: [
+      { title: "Episode 1", link: "https://drive.google.com/file/d/110UyhRjYnSOomo3-9Wh30BZonjVHr7Nu/preview" },
+      { title: "Episode 2", link: "https://drive.google.com/file/d/1128cXGsLQ_wUcjls8hcZbfy-YbeRwChc/preview" },
+      { title: "Episode 3", link: "https://drive.google.com/file/d/110VFc93Wq49TvnsWq9AclCCsyt5scLSC/preview" },
+      { title: "Episode 4", link: "https://drive.google.com/file/d/119B_PFF3wtTyQMu3pu4-rBpN1rrfI7dR/preview" },
+      { title: "Episode 5", link: "https://drive.google.com/file/d/116FV6qv7Hjdpkj4v7deuoactP-ee-T9A/preview" },
+      { title: "Episode 6", link: "https://drive.google.com/file/d/11A96GHkRYHQ1uwRwa4eMiVBxbqM5YcmA/preview" },
+      { title: "Episode 7", link: "https://drive.google.com/file/d/11TiSpcFJr2N8r7GPyIGgmgy2tHfnQTtL/preview" },
+      { title: "Episode 8", link: "https://drive.google.com/file/d/11tAEoUZsUeQa4ja3-Nnt76nOKR2_w5wa/preview" },
+      { title: "Episode 9", link: "https://drive.google.com/file/d/11V8IleXjjcnlUAparkf44XBFEUwxK8Ou/preview" },
+      { title: "Episode 10", link: "https://drive.google.com/file/d/11uRZmLQxY52jeIsQKiw6vf2d1jTiCBlr/preview" },
+    ]
+  },
+];
