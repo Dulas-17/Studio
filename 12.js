7d09341769b72ebc2188dbc43bde9402ada39a26
Dulas <abdullahtshabalala@gmail.com>
2025-06-02 16:20:08 +0200

    Create 12.js

---
+++ 12.js
@@ -0,0 +1,24 @@
+// movies.js
+const movieData = [
+  {
+    title: "Your Name(sub)",
+    image: "Images/Cover3.jpeg",
+    description: "A beautiful romantic fantasy that transcends time and space.",
+    genres: ["Animation", "Romance", "Fantasy", "Drama"],
+    link: "https://drive.google.com/file/d/10e007gPw7-zykD4XnHU_v_HV3mS7Y72Y/preview"
+  },
+  {
+    title: "The Life List",
+    image: "Images/Cover1.jpeg",
+    description: "After her mother's passing Alex is forced to complete a list she compiled when she was 13 only so she could have some of her mother's inheritance, but on her Journey she finds secrets about herself.",
+    genres: ["Drama", "Family", "Adventure"],
+    link: "https://drive.google.com/file/d/12tRqoJ9sXycE7RODnlxu11HVR7wcZbtL/preview"
+  },
+  {
+    title: "Power Rangers",
+    image: "Images/Cover2.jpeg",
+    description: "Five ordinary teens must become something extraordinary when they learn that their small town of Angel Grove – and the world – is about to be annihilated by an alien threat. Chosen by destiny, our heroes quickly discover they are the only ones who can save the planet. But to do so, they will have to overcome their real-world issues and band together as the Power Rangers before it’s too late.",
+    genres: ["Action", "Sci-Fi", "Adventure"],
+    link: "https://drive.google.com/file/d/17looZTle9cX9De1f6gN5QUcBhopDoKvx/preview"
+  },
+];
